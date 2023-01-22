15a39c50374e36442f93e29522fae5f865864ca6
DrazZ34 <jfvillac@hotmail.com>
2023-01-22 16:51:09 +0100

    menu burger

--- common/navigation.php
+++ common/navigation.php
@@ -2,6 +2,14 @@
 
     <h1><span>J</span>ean-<span>F</span>rançois <span>V</span>ILLAC</h1>
 
+    <button type="button" class="nav-toggler" aria-label="toggle curtain navigation">
+
+        <span class="line l1"></span>
+        <span class="line l2"></span>
+        <span class="line l3"></span>
+
+    </button>
+
     <nav>
 
         <ul class="liste-items">
